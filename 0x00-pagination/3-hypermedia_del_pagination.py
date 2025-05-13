8f7b05331ee5ed9335e7849c151aa71eca191c28
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-05-13 17:22:00 +0100

    3-another_shot_2

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -53,8 +53,7 @@ class Server:
                 data.append(indexed_data[current_index])
             current_index += 1
 
-        next_index = {
-            current_index if current_index < len(indexed_data) else None}
+        next_index = current_index if current_index < len(indexed_data) else None
 
         return {
             "index": index,
