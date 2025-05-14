0c1e9f2917442752345cf01eee0d1ae65509198b
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-05-14 18:26:48 +0100

    0-task

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 """0. Basic dictionary"""
 
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Basic Caching System
+    """
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if (key is not None and item is not None):
+            self.cache_date[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
